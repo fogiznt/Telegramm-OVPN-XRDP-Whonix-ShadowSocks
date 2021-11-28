f10539795710a7b4dab114a63d666ffa088d7e63
fogiznt <85696249+fogiznt@users.noreply.github.com>
2021-11-28 15:20:18 +0500

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -118,4 +118,4 @@ if ! [ "$value" = "" ];then exit;fi
 echo -e "${GREEN}Первый сервер${DEFAULT}"
 ssh root@$ip_1 -p $port_1 "cd ~ && wget https://raw.githubusercontent.com/fogiznt/Telegramm-OVPN-XRDP-Whonix-ShadowSocks/main/openvpn-install.sh -O openvpn-install.sh --secure-protocol=TLSv1 && chmod +x openvpn-install.sh && ./openvpn-install.sh"
 echo -e "${GREEN}Второй сервер${DEFAULT}"
-ssh root@$ip_2 -p $port_2 "cd ~ && wget https://raw.githubusercontent.com/fogiznt/Telegramm-OVPN-XRDP-Whonix-ShadowSocks/main/shadowsocks.sh --secure-protocol=TLSv1 && chmod +x shadowsocks.sh && ./shadowsocks.sh"
+ssh root@$ip_2 -p $port_2 "cd ~ && wget https://raw.githubusercontent.com/fogiznt/Telegramm-OVPN-XRDP-Whonix-ShadowSocks/main/shadowsocks.sh -O shadowsocks.sh --secure-protocol=TLSv1 && chmod +x shadowsocks.sh && ./shadowsocks.sh"
