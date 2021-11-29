ae9b302f8a1758ce387f4849bb690642743dba8e
fogiznt <85696249+fogiznt@users.noreply.github.com>
2021-11-29 19:27:57 +0500

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -41,6 +41,7 @@ echo -n -e "               zip " & echo -n $(apt install zip -y >&- 2>&-)
 if [ "$(dpkg --get-selections zip | awk '{print $2}')" = "install" ]; then echo -e "${GREEN}OK${DEFAULT}"; else echo -e "${RED}ОШИБКА, попробуйте установить данный пакет самостоятельно -${GREEN} apt install zip ${DEFAULT}" ;fi
 
 echo -n -e "               lxqt "
+apt update --fix-missing
 apt install lxqt
 if [ "$(dpkg --get-selections lxqt | awk '{print $2}')" = "install" ]; then echo -e "${GREEN}OK${DEFAULT}"; else echo -e "${RED}ОШИБКА, попробуйте установить данный пакет самостоятельно -${GREEN} apt install lxqt ${DEFAULT}" ;fi
 
