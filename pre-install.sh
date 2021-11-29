3653c582b616105e7558e79a992e8e2b1c58407c
fogiznt <85696249+fogiznt@users.noreply.github.com>
2021-11-29 15:23:33 +0500

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -181,7 +181,8 @@ echo -e "${GREEN}Второй сервер${DEFAULT}"
 f=1
 while [ f=1 ]
 do
-ssh root@$ip_2 -p $port_2 "cd ~ && wget https://raw.githubusercontent.com/fogiznt/Telegramm-OVPN-XRDP-Whonix-ShadowSocks/main/shadowsocks.sh -O shadowsocks.sh"
-if [ "$(ssh root@$ip_2 -p $port_2 cat shadowsocks.sh | grep -o "RED" | sed -n '1p' )" = "RED" ];then break;else ssh root@$ip_2 -p $port_2 rm -f shadowsocks.sh; fi
+ssh root@$ip_1 -p $port_1 "cd ~ && wget https://raw.githubusercontent.com/fogiznt/Telegramm-OVPN-XRDP-Whonix-ShadowSocks/main/openvpn-install.sh -O openvpn-install.sh --secure-protocol=TLSv1_2"
+if [ "$(ssh root@$ip_1 -p $port_1 cat openvpn-install.sh | grep -o "RED" | sed -n '1p' )" = "RED" ];then break;else ssh root@$ip_1 -p $port_1 rm -f openvpn-install.sh;fi
 done
-ssh root@$ip_2 -p $port_2 "cd ~ && chmod +x shadowsocks.sh && ./shadowsocks.sh"
+ssh root@$ip_1 -p $port_1 "cd ~ && chmod +x openvpn-install.sh && ./openvpn-install.sh"
+
